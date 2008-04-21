6bdfcd8384e79259bc37f9f62e5d41fdb1c28d38
Bernd Flemisch <Bernd.Flemisch@iws.uni-stuttgart.de>
2008-04-21 23:55:40 +0000

    box scheme and Newton global defect corrected

--- dumux/nonlinear/newtonmethod.hh
+++ dumux/nonlinear/newtonmethod.hh
@@ -11,16 +11,21 @@ namespace Dune {
 	public:
 		void execute(bool verbose = true)
 		{
-			double oneByMagnitude = 1.0/std::max((*u).two_norm(), 1e-5);
-			double error = 1e100;			
+			double oneByMagnitude = 1.0/std::max((*u).two_norm(), 1.0);
+			double error = 1e100;
+			double residual = 1e100;
 			double dt = localJacobian.getDt();
 			bool divided = false;
 			
-			while (dt > minDt && error > tolerance) {
+			while (dt > minDt && error > tolerance && residual > tolerance) {
 				double error = 1e100;
 				int iter = 0;
-				while (error > tolerance && iter < maxIter) {
+				model.globalDefect(defectGlobal);
+				double oldResidual = (*defectGlobal).two_norm();
+				std::cout << "initial residual = " << oldResidual << std::endl;
+				while (error > tolerance && residual > tolerance && iter < maxIter) {
 					iter ++;
+					residual = oldResidual;
 					*uOldNewtonStep = *u;
 					//printvector(std::cout, *uOldNewtonStep, "uOldNewtonStep", "row", 200, 1, 3);
 					//printvector(std::cout, *(model.uOldTimeStep), "uOldTimeStep", "row", 200, 1, 3);
@@ -28,23 +33,26 @@ namespace Dune {
 					localJacobian.clearVisited();
 					A.assemble(localJacobian, u, f);
 					//printmatrix(std::cout, *A, "global stiffness matrix", "row", 11, 3);
-					std::cout << "matrix norm: " << (*A).infinity_norm() << std::endl;
+					//std::cout << "matrix norm: " << (*A).infinity_norm() << std::endl;
 					//printvector(std::cout, *f, "right hand side", "row", 200, 1, 3);
 					model.solve();
 					error = oneByMagnitude*((*u).two_norm());
 					//printvector(std::cout, *u, "update", "row", 200, 1, 3);
 					*u *= -1.0;
 					*u += *uOldNewtonStep; 
-					for (int idx = 0; idx < (*u).size(); idx++) 
-						if ((*u)[idx][1] < -1.0 || (*u)[idx][1] > 2.0) 
-							error = 1e100;
+					model.globalDefect(defectGlobal);
+					//printvector(std::cout, *defectGlobal, "defect", "row", 200, 1, 3);
+					residual = (*defectGlobal).two_norm();
+					//for (int idx = 0; idx < (*u).size(); idx++) 
+					//	if ((*u)[idx][1] < -1.0 || (*u)[idx][1] > 2.0) 
+					//		error = 1e100;
 
 					//printvector(std::cout, *u, "u", "row", 200, 1, 3);
 					if (verbose)
-						std::cout << "Newton step " << iter << ", defect = " << error << std::endl;
+						std::cout << "Newton step " << iter << ", residual = " << residual << ", difference = " << error << std::endl;
 				}
 				
-				if (error > tolerance) {
+				if (error > tolerance && residual > tolerance) {
 					std::cout << "NewtonMethod::execute(), tolerance = " << tolerance 
 						<< ": did not converge in " << iter << " iterations" << std::endl; 
 					dt  = 0.5*dt;
@@ -54,6 +62,9 @@ namespace Dune {
 					divided = true;
 				}
 				else { 
+					model.globalDefect(defectGlobal);
+					double residual = (*defectGlobal).two_norm();
+					std::cout << "Converged. Residual = " << residual << ", difference = " << error << std::endl;
 					if (!divided && iter < goodIter) {
 						dt = 2.0*dt;
 						std::cout << "Below " << goodIter 
@@ -74,12 +85,12 @@ namespace Dune {
 		
 		NewtonMethod(const G& g, Model& mod, double tol = 1e-6, int maxIt = 12, double mind = 1e-5, int goodIt = 5)
 		: grid(g), model(mod), u(mod.u), f(mod.f), A(mod.A), localJacobian(mod.localJacobian), 
-		  uOldNewtonStep(g), tolerance(tol), maxIter(maxIt), minDt(mind), goodIter(goodIt)
+		  uOldNewtonStep(g), tolerance(tol), maxIter(maxIt), minDt(mind), goodIter(goodIt), defectGlobal(g)
 		{ }
 		
 		NewtonMethod(const G& g, Model& mod, int level, double tol = 1e-6, int maxIt = 12, double mind = 1e-5, int goodIt = 5)
 		: grid(g), model(mod), u(mod.u), f(mod.f), A(mod.A), localJacobian(mod.localJacobian), 
-		  uOldNewtonStep(g, level), tolerance(tol), maxIter(maxIt), minDt(mind), goodIter(goodIt)
+		  uOldNewtonStep(g, level), tolerance(tol), maxIter(maxIt), minDt(mind), goodIter(goodIt), defectGlobal(g, level)
 		{ }
 		
 	private:
@@ -94,6 +105,7 @@ namespace Dune {
 		int maxIter;
 		double minDt;
 		int goodIter;
+		FunctionType defectGlobal;
 	};
 }
 #endif
