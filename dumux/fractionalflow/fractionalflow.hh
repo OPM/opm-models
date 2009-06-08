975b339e6f8e8d39fb496afffffa82527e7f761f
Markus Wolff <Markus.Wolff@iws.uni-stuttgart.de>
2009-06-08 09:41:49 +0000

    bug concerning potentials at a neumann boundary fixed in fvpressure2p.hh

--- dumux/fractionalflow/fractionalflow.hh
+++ dumux/fractionalflow/fractionalflow.hh
@@ -1,5 +1,19 @@
 // $Id$
-
+/*****************************************************************************
+ *   Copyright (C) 2007-2009 by Bernd Flemisch                               *
+ *   Copyright (C) 2008-2009 by Markus Wolff                                 *
+ *   Institute of Hydraulic Engineering                                      *
+ *   University of Stuttgart, Germany                                        *
+ *   email: <givenname>.<name>@iws.uni-stuttgart.de                          *
+ *                                                                           *
+ *   This program is free software; you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation; either version 2 of the License, or       *
+ *   (at your option) any later version, as long as this copyright notice    *
+ *   is included in its original form.                                       *
+ *                                                                           *
+ *   This program is distributed WITHOUT ANY WARRANTY.                       *
+ *****************************************************************************/
 #ifndef DUNE_FRACTIONALFLOW_HH
 #define DUNE_FRACTIONALFLOW_HH
 
@@ -9,7 +23,7 @@
 /**
  * @file
  * @brief  Base class for defining an instance of a numerical two phase flow model
- * @author Bernd Flemisch, last changed by Markus Wolff
+ * @author Bernd Flemisch, Markus Wolff
  */
 
 /**
