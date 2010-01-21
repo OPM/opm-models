115f9778c84a74e24fa6b0f984ac6df6a57f339b
Markus Wolff <Markus.Wolff@iws.uni-stuttgart.de>
2010-01-21 07:57:28 +0000

    ...

---
+++ dumux/new_decoupled/2p/diffusion/diffusion.hh
@@ -0,0 +1,144 @@
+// $Id$
+/*****************************************************************************
+ *   Copyright (C) 2007-2009 by Bernd Flemisch                               *
+ *   Copyright (C) 2008-2009 by Markus Wolff                                 *
+ *   Institute of Hydraulic Engineering                                      *
+ *   University of Stuttgart, Germany                                        *
+ *   email: <givenname>.<name>@iws.uni-stuttgart.de                          *
+ *                                                                           *
+ *   This program is free software; you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation; either version 2 of the License, or       *
+ *   (at your option) any later version, as long as this copyright notice    *
+ *   is included in its original form.                                       *
+ *                                                                           *
+ *   This program is distributed WITHOUT ANY WARRANTY.                       *
+ *****************************************************************************/
+#ifndef DUNE_DIFFUSION_HH
+#define DUNE_DIFFUSION_HH
+
+#include "dumux/diffusion/diffusionproblem.hh"
+
+/**
+ * @file
+ * @brief  Base class for defining an instance of a numerical diffusion model
+ * @author Bernd Flemisch, Markus Wolff
+ */
+
+/*!
+ * \ingroup fracflow
+ * \defgroup diffusion Diffusion
+ */
+
+namespace Dune
+{
+//! \ingroup diffusion
+//! Base class for defining an instance of a numerical diffusion model.
+/*! An interface for defining a numerical diffusion model for the
+ *  solution of equations of the form
+ *  \f[\text{div}\, \boldsymbol{v} = q,\f]
+ *  where, the velocity \f$\boldsymbol{v} \sim \boldsymbol{K} \nabla p \f$,
+ *  \f$p\f$ is a pressure and \f$q\f$ a source/sink term
+
+ Template parameters are:
+
+ - GridView      a DUNE gridview type
+ - Scalar        type used for scalar quantities
+ - VC            type of a class containing different variables of the model
+ - Problem       class defining the physical problem
+*/
+template<class GridView, class Scalar, class VC, class Problem = DiffusionProblem<GridView, Scalar, VC> >
+class Diffusion
+{
+public:
+    typedef Scalar ScalarType;
+    typedef Problem ProblemType;
+
+    virtual void pressure(const Scalar t=0)
+    {
+    	return;
+    }
+
+    	//! Calculate the pressure.
+    /*!
+     *  \param t time
+     *
+     *  Calculates the pressure \f$p\f$ as solution of the boundary value problem
+     *  \f[  \text{div}\, \boldsymbol{v} = q, \f]
+     *  subject to appropriate boundary conditions.
+     */
+    virtual void pressure(bool first, const Scalar t = 0)
+    {
+        return pressure(t);
+    }
+
+    //! Calculate the velocity.
+    /*!
+     *  \param t time
+     *
+     *
+     *  Given the piecewise constant pressure \f$p\f$,
+     *  this method calculates the velocity
+     *  The method is needed in the IMPES (Implicit Pressure Explicit Saturation) algorithm which is used for a fractional flow formulation
+     *  to provide the velocity field required for the solution of the saturation equation.
+     */
+    virtual void calculateVelocity(const Scalar t = 0)
+    {
+        return;
+    }
+
+    //! Start a post-processing procedure at the end of a timestep.
+    /*!
+     *  \param t time
+     *  \param dt time step
+     *
+     *  If an explicit <it>Euler</it> time discretization is used this function will be called
+     *  at the end of each time step.
+     */
+    virtual void postProcessUpdate(Scalar t, Scalar dt)
+    {
+        return;
+    }
+
+    //! Returns a reference to the problem
+    virtual Problem& problem()
+    {
+        return diffProblem;
+    }
+    virtual const Problem& problem() const
+    {
+        return diffProblem;
+    }
+
+    GridView& getGridView()
+    {
+        return gridView;
+    }
+
+    const GridView& getGridView() const
+    {
+        return gridView;
+    }
+
+    //! always define virtual destructor in abstract base class
+    virtual ~Diffusion()
+    {
+    }
+
+    //! Constructs a Diffusion object
+    /**
+     * \param grid gView object of type GridView
+     * \param prob a problem class object
+     */
+    Diffusion(const GridView& gView, Problem& problem) :
+        gridView(gView), diffProblem(problem)
+    {
+    }
+
+protected:
+    const GridView& gridView; //!< object of type Dune::GridView
+    Problem& diffProblem; //!< problem data
+};
+
+}
+#endif
---
+++ dumux/new_decoupled/2p/transport/transport.hh
@@ -0,0 +1,176 @@
+// $Id$
+/*****************************************************************************
+ *   Copyright (C) 2007-2009 by Bernd Flemisch                               *
+ *   Copyright (C) 2008-2009 by Markus Wolff                                 *
+ *   Institute of Hydraulic Engineering                                      *
+ *   University of Stuttgart, Germany                                        *
+ *   email: <givenname>.<name>@iws.uni-stuttgart.de                          *
+ *                                                                           *
+ *   This program is free software; you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation; either version 2 of the License, or       *
+ *   (at your option) any later version, as long as this copyright notice    *
+ *   is included in its original form.                                       *
+ *                                                                           *
+ *   This program is distributed WITHOUT ANY WARRANTY.                       *
+ *****************************************************************************/
+#ifndef DUNE_TRANSPORT_HH
+#define DUNE_TRANSPORT_HH
+
+#include "dumux/transport/transportproblem.hh"
+
+/**
+ * @file
+ * @brief  Base class for defining an instance of a numerical transport model
+ * @author Bernd Flemisch, Markus Wolff
+ */
+
+/*!
+ * \ingroup fracflow
+ * \defgroup transport Transport
+ */
+
+namespace Dune
+{
+//! \ingroup transport
+//! Base class for defining an instance of a numerical transport model.
+/*! An interface for defining a numerical transport model for the
+ *  solution of equations of the form
+ *  \f[
+ *    \frac{\partial S}{\partial t} + \text{div}\, \boldsymbol{v_{\alpha}} = 0,
+ *  \f]
+ *  where \f$S\f$ denotes a phase saturation and \f$\boldsymbol{v_{\alpha}}\f$ is a phase velocity.
+
+ Template parameters are:
+
+ - GridView      a DUNE gridview type
+ - Scalar        type used for scalar quantities
+ - VC            type of a class containing different variables of the model
+ - Problem       class defining the physical problem
+
+*/
+template<class GridView, class Scalar, class VC, class Problem =  TransportProblem<GridView, Scalar, VC> >
+class Transport
+{
+public:
+    typedef    typename VC::ScalarVectorType RepresentationType;//!< Data type for a Vector of Scalars
+
+    //! Calculate the update vector.
+    /*!
+     *  \param[in]  t         time
+     *  \param[in] dt         time step size
+     *  \param[in] updateVec  vector for the update values
+     *  \param[in] CLFFac     security factor for the time step criterion (0 < CLFFac <= 1)
+     *  \param[in] impes      variable is true if an impes algorithm is used and false if the transport part is solved independently
+     *
+     *  Calculate the update vector
+     */
+    virtual int update(const Scalar t, Scalar& dt, RepresentationType& updateVec, Scalar& CLFFac, bool impes) = 0;
+
+    //! Call the initialization function.
+    /*!
+     *  Call the initialization function -> called from Dune::TimeLoop if the transport part is solved independently
+     */
+    void initial()
+    {
+        initialTransport();
+        return;
+    }
+
+    //! Sets the initial solution \f$S_0\f$.
+    virtual void initialTransport() = 0;
+
+    //! return const reference to saturation vector
+    virtual const RepresentationType& operator* () const
+    {
+        return transProblem.variables().saturation();
+    }
+
+    //! return reference to saturation vector
+    virtual RepresentationType& operator* ()
+    {
+        return transProblem.variables().saturation();
+    }
+
+    //! Start a post-processing procedure at the end of a timestep.
+    /*!
+     *  \param t time
+     *  \param dt time step
+     *
+     *  If an explicit Euler time discretization is used this function will be called
+     *  at the end of each time step.
+     */
+    virtual void postProcessUpdate(Scalar t, Scalar dt)
+    {
+        return;
+    }
+
+    //! Update the values of the material laws and constitutive relations.
+    /*!
+     *  Constitutive relations like capillary pressure-saturation relationships, mobility-saturation relationships... are updated and stored in the variable class
+     *  of type Dune::VariableClass2P. The update has to be done when new saturation are available.
+     */
+    virtual void updateMaterialLaws(RepresentationType& saturation, bool iterate)
+    {
+        return;
+    }
+
+    //! Returns a reference to the problem
+    Problem& problem()
+    {
+        return transProblem;
+    }
+    //! Returns a reference to the problem
+    const Problem& problem() const
+    {
+        return transProblem;
+    }
+
+    // serialization methods
+    template <class Restarter>
+    void serialize(Restarter &res)
+    {
+        transProblem.variables().serialize<Restarter>(res);
+    }
+    template <class Restarter>
+    void deserialize(Restarter &res)
+    {
+        transProblem.variables().deserialize<Restarter>(res);
+    }
+    
+    GridView& getGridView()
+    {
+        return gridView;
+    }
+
+    const GridView& getGridView() const
+    {
+        return gridView;
+    }
+
+    //! \brief Write data files
+    /*!
+     *  \param name file name
+     *  \param k format parameter
+     */
+    virtual void vtkout(const char* name, int k)const = 0;
+
+    //! always define virtual destructor in abstract base class
+    virtual ~Transport ()
+    {}
+
+    /*! @brief Constructs a Transport object
+     *  @param gridView a DUNE gridview object
+     *  @param prob an object of class TransportProblem or derived
+     */
+    Transport(const GridView& gridView, Problem& problem)
+        : gridView(gridView), transProblem(problem)
+    {}
+
+protected:
+    const GridView& gridView; //!< object of type Dune::GridView
+    Problem& transProblem; //!< problem data
+};
+
+}
+#endif
