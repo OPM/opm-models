b38cee64c64cdfeeda746a8b638f19f0dad64ea2
Bernd Flemisch <Bernd.Flemisch@iws.uni-stuttgart.de>
2010-05-26 13:00:47 +0000

    made mimetic method independent of CRshapefunctions

--- test/new_decoupled/test_diffusion_problem.hh
+++ test/new_decoupled/test_diffusion_problem.hh
@@ -1,20 +1,20 @@
 // $Id$
 /*****************************************************************************
- *   Copyright (C) 2007-2008 by Klaus Mosthaf                                *
- *   Copyright (C) 2007-2008 by Bernd Flemisch                               *
- *   Copyright (C) 2008-2009 by Andreas Lauser                               *
- *   Institute of Hydraulic Engineering                                      *
- *   University of Stuttgart, Germany                                        *
- *   email: <givenname>.<name>@iws.uni-stuttgart.de                          *
- *                                                                           *
- *   This program is free software; you can redistribute it and/or modify    *
- *   it under the terms of the GNU General Public License as published by    *
- *   the Free Software Foundation; either version 2 of the License, or       *
- *   (at your option) any later version, as long as this copyright notice    *
- *   is included in its original form.                                       *
- *                                                                           *
- *   This program is distributed WITHOUT ANY WARRANTY.                       *
- *****************************************************************************/
+*   Copyright (C) 2007-2008 by Klaus Mosthaf                                *
+*   Copyright (C) 2007-2008 by Bernd Flemisch                               *
+*   Copyright (C) 2008-2009 by Andreas Lauser                               *
+*   Institute of Hydraulic Engineering                                      *
+*   University of Stuttgart, Germany                                        *
+*   email: <givenname>.<name>@iws.uni-stuttgart.de                          *
+*                                                                           *
+*   This program is free software; you can redistribute it and/or modify    *
+*   it under the terms of the GNU General Public License as published by    *
+*   the Free Software Foundation; either version 2 of the License, or       *
+*   (at your option) any later version, as long as this copyright notice    *
+*   is included in its original form.                                       *
+*                                                                           *
+*   This program is distributed WITHOUT ANY WARRANTY.                       *
+*****************************************************************************/
 #ifndef DUMUX_TEST_2P_PROBLEM_HH
 #define DUMUX_TEST_2P_PROBLEM_HH
 
@@ -65,9 +65,9 @@ public:
 
 SET_PROP(DiffusionTestProblem, Model)
 {
-//    typedef Dumux::FVPressure2P<TTAG(DiffusionTestProblem)> type;
+    //    typedef Dumux::FVPressure2P<TTAG(DiffusionTestProblem)> type;
     typedef Dumux::MimeticPressure2P<TTAG(DiffusionTestProblem)> type;
-//    typedef Dumux::FVMPFAOPressure2P<TTAG(DiffusionTestProblem)> type;
+    //    typedef Dumux::FVMPFAOPressure2P<TTAG(DiffusionTestProblem)> type;
 };
 
 //SET_INT_PROP(DiffusionTestProblem, VelocityFormulation,
@@ -110,133 +110,129 @@ SET_BOOL_PROP(DiffusionTestProblem, EnableGravity, false);
 }
 
 /*!
- * \ingroup DecoupledProblems
- */
+* \ingroup DecoupledProblems
+*/
 template<class TypeTag = TTAG(DiffusionTestProblem)>
 class TestDiffusionProblem: public DiffusionProblem2P<TypeTag, TestDiffusionProblem<TypeTag> >
 {
-typedef TestDiffusionProblem<TypeTag> ThisType;
-typedef DiffusionProblem2P<TypeTag, ThisType> ParentType;
-typedef typename GET_PROP_TYPE(TypeTag, PTAG(GridView)) GridView;
+    typedef TestDiffusionProblem<TypeTag> ThisType;
+    typedef DiffusionProblem2P<TypeTag, ThisType> ParentType;
+    typedef typename GET_PROP_TYPE(TypeTag, PTAG(GridView)) GridView;
 
-typedef typename GET_PROP_TYPE(TypeTag, PTAG(TwoPIndices)) Indices;
+    typedef typename GET_PROP_TYPE(TypeTag, PTAG(TwoPIndices)) Indices;
 
-typedef typename GET_PROP_TYPE(TypeTag, PTAG(FluidSystem)) FluidSystem;
-typedef typename GET_PROP_TYPE(TypeTag, PTAG(FluidState)) FluidState;
+    typedef typename GET_PROP_TYPE(TypeTag, PTAG(FluidSystem)) FluidSystem;
+    typedef typename GET_PROP_TYPE(TypeTag, PTAG(FluidState)) FluidState;
 
-enum
-{
-    dim = GridView::dimension, dimWorld = GridView::dimensionworld
-};
+    enum
+    {
+        dim = GridView::dimension, dimWorld = GridView::dimensionworld
+    };
 
-enum
-{
-    wPhaseIdx = Indices::wPhaseIdx, nPhaseIdx = Indices::nPhaseIdx
-};
+    enum
+    {
+        wPhaseIdx = Indices::wPhaseIdx, nPhaseIdx = Indices::nPhaseIdx
+    };
 
-typedef typename GET_PROP_TYPE(TypeTag, PTAG(Scalar)) Scalar;
+    typedef typename GET_PROP_TYPE(TypeTag, PTAG(Scalar)) Scalar;
 
-typedef typename GridView::Traits::template Codim<0>::Entity Element;
-typedef typename GridView::Intersection Intersection;
-typedef Dune::FieldVector<Scalar, dimWorld> GlobalPosition;
-typedef Dune::FieldVector<Scalar, dim> LocalPosition;
+    typedef typename GridView::Traits::template Codim<0>::Entity Element;
+    typedef typename GridView::Intersection Intersection;
+    typedef Dune::FieldVector<Scalar, dimWorld> GlobalPosition;
+    typedef Dune::FieldVector<Scalar, dim> LocalPosition;
 
 public:
-TestDiffusionProblem(const GridView &gridView, const GlobalPosition lowerLeft = 0, const GlobalPosition upperRight = 0) :
-ParentType(gridView), lowerLeft_(lowerLeft), upperRight_(upperRight)
-{
-    this->variables().saturation()=1;
-}
-
-/*!
- * \name Problem parameters
- */
-// \{
-
-/*!
- * \brief The problem name.
- *
- * This is used as a prefix for files generated by the simulation.
- */
-const char *name() const
-{
-    return "testdiffusion";
-}
-
-bool shouldWriteRestartFile() const
-{
-    return false;
-}
-
-/*!
- * \brief Returns the temperature within the domain.
- *
- * This problem assumes a temperature of 10 degrees Celsius.
- */
-Scalar temperature(const GlobalPosition& globalPos, const Element& element) const
-{
-    return 273.15 + 10; // -> 10°C
-}
-
-// \}
-
-Scalar referencePressure(const GlobalPosition& globalPos, const Element& element) const
-{
-    return 1e5; // -> 10°C
-}
-
-std::vector<Scalar> source(const GlobalPosition& globalPos, const Element& element)
-{
-    return std::vector<Scalar>(2, 0.0);
-}
-
-typename BoundaryConditions::Flags bctypePress(const GlobalPosition& globalPos, const Intersection& intersection) const
-{
-    if ((globalPos[0] < eps_) || globalPos[0] > upperRight_[0] - eps_)
-    return BoundaryConditions::dirichlet;
-    // all other boundaries
-    return BoundaryConditions::neumann;
-}
-
-BoundaryConditions::Flags bctypeSat(const GlobalPosition& globalPos, const Intersection& intersection) const
-{
-    //        if (globalPos[0] > (upperRight_[0] - eps_) || globalPos[0] < eps_)
-    if (globalPos[0] < eps_ || globalPos[0] > upperRight_[0] - eps_)
-    return Dumux::BoundaryConditions::dirichlet;
-    else
-    return Dumux::BoundaryConditions::neumann;
-}
-
-Scalar dirichletPress(const GlobalPosition& globalPos, const Intersection& intersection) const
-{
-    if (globalPos[0] < eps_)
+    TestDiffusionProblem(const GridView &gridView, const GlobalPosition lowerLeft = 0, const GlobalPosition upperRight = 0) :
+        ParentType(gridView), lowerLeft_(lowerLeft), upperRight_(upperRight)
     {
-        return 2e5;
+        this->variables().saturation()=1;
     }
-    // all other boundaries
-    return 1.99e5;
-}
 
-Scalar dirichletSat(const GlobalPosition& globalPos, const Intersection& intersection) const
-{
-    return 1;
-}
+    /*!
+    * \name Problem parameters
+    */
+    // \{
 
-std::vector<Scalar> neumannPress(const GlobalPosition& globalPos, const Intersection& intersection) const
-{
-    std::vector<Scalar> neumannFlux(2, 0.0);
-    if (globalPos[0] > upperRight_[0] - eps_)
+    /*!
+    * \brief The problem name.
+    *
+    * This is used as a prefix for files generated by the simulation.
+    */
+    const char *name() const
     {
-        neumannFlux[nPhaseIdx] = 5e-5;
+        return "testdiffusion";
     }
-    return neumannFlux;
-}
+
+    bool shouldWriteRestartFile() const
+    {
+        return false;
+    }
+
+    /*!
+    * \brief Returns the temperature within the domain.
+    *
+    * This problem assumes a temperature of 10 degrees Celsius.
+    */
+    Scalar temperature(const GlobalPosition& globalPos, const Element& element) const
+    {
+        return 273.15 + 10; // -> 10°C
+    }
+
+    // \}
+
+    Scalar referencePressure(const GlobalPosition& globalPos, const Element& element) const
+    {
+        return 1e5; // -> 10°C
+    }
+
+    std::vector<Scalar> source(const GlobalPosition& globalPos, const Element& element)
+        {
+        return std::vector<Scalar>(2, 0.0);
+        }
+
+    typename BoundaryConditions::Flags bctypePress(const GlobalPosition& globalPos, const Intersection& intersection) const
+    {
+        if ((globalPos[0] < lowerLeft_[0] + eps_) || globalPos[0] > upperRight_[0] - eps_)
+            return BoundaryConditions::dirichlet;
+        // all other boundaries
+        return BoundaryConditions::neumann;
+    }
+
+    BoundaryConditions::Flags bctypeSat(const GlobalPosition& globalPos, const Intersection& intersection) const
+    {
+        if (globalPos[0] < lowerLeft_[0] + eps_ || globalPos[0] > upperRight_[0] - eps_)
+            return Dumux::BoundaryConditions::dirichlet;
+        else
+            return Dumux::BoundaryConditions::neumann;
+    }
+
+    Scalar dirichletPress(const GlobalPosition& globalPos, const Intersection& intersection) const
+    {
+        if (globalPos[0] < lowerLeft_[0] + eps_)
+        {
+            return 2e5;
+        }
+        // all other boundaries
+        return 1.99e5;
+    }
+
+    Scalar dirichletSat(const GlobalPosition& globalPos, const Intersection& intersection) const
+    {
+        return 1;
+    }
+
+    std::vector<Scalar> neumannPress(const GlobalPosition& globalPos, const Intersection& intersection) const
+    {
+        std::vector<Scalar> neumannFlux(2, 0.0);
+
+        return neumannFlux;
+    }
 
 private:
-GlobalPosition lowerLeft_;
-GlobalPosition upperRight_;
+    GlobalPosition lowerLeft_;
+    GlobalPosition upperRight_;
 
-static const Scalar eps_ = 1e-6;
+    static const Scalar eps_ = 1e-6;
 };
 } //end namespace
 
