a7cc40d6bea2e6a05a43ac7eff6bba1f40c45afd
Andreas Lauser <and@poware.org>
2012-08-23 12:14:32 +0200

    remove IWR copyright notices, add missing emacs and vi modelines
    the IWR copyright was valid when this stuff was based on
    dune-disc. since there is not a single line left of the original code,
    they can be removed.

--- dumux/freeflow/stokes/stokesmodel.hh
+++ dumux/freeflow/stokes/stokesmodel.hh
@@ -1,16 +1,6 @@
 // -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
 // vi: set et ts=4 sw=4 sts=4:
 /*****************************************************************************
- *   Copyright (C) 2007 by Peter Bastian                                     *
- *   Institute of Parallel and Distributed System                            *
- *   Department Simulation of Large Systems                                  *
- *   University of Stuttgart, Germany                                        *
- *                                                                           *
- *   Copyright (C) 2010-2012 by Katherina Baber, Klaus Mosthaf               *
- *   Copyright (C) 2008-2009 by Bernd Flemisch, Andreas Lauser               *
- *   Institute for Modelling Hydraulic and Environmental Systems             *
- *   University of Stuttgart, Germany                                        *
- *   email: <givenname>.<name>@iws.uni-stuttgart.de                          *
  *                                                                           *
  *   This program is free software: you can redistribute it and/or modify    *
  *   it under the terms of the GNU General Public License as published by    *
