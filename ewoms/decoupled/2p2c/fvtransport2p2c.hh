75f632ed96ae1f2da004779b43f5dbaf42e07872
Bernd Flemisch <Bernd.Flemisch@iws.uni-stuttgart.de>
2012-10-23 15:27:09 +0000

    Enable parallelism for decoupled 2p2c. Requires AMGBackend and patches for pdelab and istl. Can therefore only be used from devel (currently). Reviewed by Benjamin.
    Dumux-Svn-Revison: 9414
    Ported-By: Andreas Lauser <andreas.lauser@iws.uni-stuttgart.de>

--- ewoms/decoupled/2p2c/fvtransport2p2c.hh
+++ ewoms/decoupled/2p2c/fvtransport2p2c.hh
@@ -24,6 +24,7 @@
 #include <ewoms/decoupled/constraintsolvers/impetflash.hh>
 #include <dune/grid/common/gridenums.hh>
 #include <ewoms/common/math.hh>
+#include <ewoms/linear/vectorexchange.hh>
 
 #include <dune/common/fvector.hh>
 #include <dune/common/fmatrix.hh>
@@ -326,6 +327,22 @@ void FVTransport2P2C<TypeTag>::update(const Scalar t, Scalar& dt,
             restrictingCell= globalIdxI;
         }
     } // end grid traversal
+
+#if HAVE_MPI
+    // communicate updated values
+    typedef typename GET_PROP(TypeTag, SolutionTypes) SolutionTypes;
+    typedef typename SolutionTypes::ElementMapper ElementMapper;
+    typedef VectorExchange<ElementMapper, Dune::BlockVector<Dune::FieldVector<Scalar, 1> > > DataHandle;
+    for (int i = 0; i < updateVec.size(); i++)
+    {
+        DataHandle dataHandle(problem_.variables().elementMapper(), updateVec[i]);
+        problem_.gridView().template communicate<DataHandle>(dataHandle,
+                                                            Dune::InteriorBorder_All_Interface,
+                                                            Dune::ForwardCommunication);
+    }
+    dt = problem_.gridView().comm().min(dt);
+#endif
+
     if(impet)
     {
         Dune::dinfo << "Timestep restricted by CellIdx " << restrictingCell << " leads to dt = "
