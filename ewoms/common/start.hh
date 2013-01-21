7fd0c12a4fa0e27a0bcb09004ff7fae9f5076b90
Andreas Lauser <Andreas.Lauser@iws.uni-stuttgart.de>
2013-01-21 11:13:33 +0100

    fix deprecation warning for the current dune trunk

--- ewoms/common/start.hh
+++ ewoms/common/start.hh
@@ -30,7 +30,12 @@
 
 #include <ewoms/common/parametersystem.hh>
 
+#if DUNE_VERSION_NEWER(DUNE_COMMON, 2, 3)
+#include <dune/common/parallel/mpihelper.hh>
+#else
 #include <dune/common/mpihelper.hh>
+#endif
+
 #include <dune/grid/io/file/dgfparser.hh>
 #include <dune/common/parametertreeparser.hh>
 
