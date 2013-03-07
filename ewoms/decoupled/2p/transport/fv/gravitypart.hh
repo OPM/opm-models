141144d158104ff602e010371123e33ca6344579
Thomas Fetzer <Thomas.Fetzer@iws.uni-stuttgart.de>
2013-03-07 18:39:23 +0000

    changed \text{grad} to \textbf{grad} in docu
    Dumux-Svn-Revison: 10355
    Ported-By: Andreas Lauser <andreas.lauser@iws.uni-stuttgart.de>

--- ewoms/decoupled/2p/transport/fv/gravitypart.hh
+++ ewoms/decoupled/2p/transport/fv/gravitypart.hh
@@ -38,7 +38,7 @@ namespace Ewoms
  * Defines the gravity term of the form
  *
  * \f[
- * \bar \lambda \boldsymbol K \, (\rho_n - \rho_w) \, g \, \text{grad} \, z,
+ * \bar \lambda \boldsymbol K \, (\rho_n - \rho_w) \, g \, \textbf{grad} \, z,
  * \f]
  *
  * where \f$ \bar \lambda = \lambda_w f_n = \lambda_n f_w \f$ and \f$ \lambda \f$ is a phase mobility and \f$ f \f$ a phase fractional flow function,
