a8dfc108f150d8421a2fb63d82e1952fcceb7985
Andreas Lauser <and@poware.org>
2013-08-12 19:45:37 +0200

    add a version.hh file

---
+++ ewoms/version.hh
@@ -0,0 +1,9 @@
+/**
+ * Current API level (for use with DUNE_VERSION_xxx):
+ */
+#define EWOMS_VERSION_MAJOR    2
+#define EWOMS_VERSION_MINOR    4
+#define EWOMS_VERSION_REVISION 0
+
+#define EWOMS_VERSION "2.4-git"
+#define EWOMS_CODENAME "Esmarelda"
