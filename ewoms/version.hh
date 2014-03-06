63d6274580f3552d0e3d1ecbfdcac4c37112f215
Andreas Lauser <and@poware.org>
2014-03-06 21:28:19 +0100

    don't distinguish between API and release version anymore
    this was too confusing IMHO, so now the API version equals the release
    version...

--- ewoms/version.hh
+++ ewoms/version.hh
@@ -1,9 +1,9 @@
 /*
  * Current API level (for use with DUNE_VERSION_xxx):
  */
-#define EWOMS_VERSION_MAJOR 2
-#define EWOMS_VERSION_MINOR 5
+#define EWOMS_VERSION_MAJOR 2014
+#define EWOMS_VERSION_MINOR 04
 #define EWOMS_VERSION_REVISION 0
 
-#define EWOMS_VERSION "2.5-git"
+#define EWOMS_VERSION "2014.04-git"
 #define EWOMS_CODENAME "Ringo"
