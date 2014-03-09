af2e99f57c498f902f5b975707c2e620e01cc6cd
Andreas Lauser <and@poware.org>
2014-03-09 14:16:43 +0100

    disable compilation of the tests by default
    they take half an eternity to compile and if you're working on an
    external module which uses ewoms, this used to be a major time sink
    which basically did not come with any benefit.
    
    If you're interested in developing the eWoms core, pass
    --enable-testing to the configure script or -DBUILD_TESTS to cmake...

--- cmake/Modules/EwomsAddTest.cmake
+++ cmake/Modules/EwomsAddTest.cmake
@@ -21,51 +21,52 @@ macro(EwomsAddTest TestName)
 
   set(BUILD_TESTING "${BUILD_TESTING}")
 
-  if (BUILD_TESTING)
-    if (NOT CURTEST_EXE_NAME)
-      set(CURTEST_EXE_NAME ${TestName})
-    endif()
-    if (NOT CURTEST_SOURCES)
-      set(CURTEST_SOURCES ${CURTEST_EXE_NAME}.cc)
-    endif()
-    if (NOT CURTEST_DRIVER_ARGS)
-      set(CURTEST_DRIVER_ARGS --simulation ${CURTEST_EXE_NAME})
-    endif()
-    
-    if (NOT DISABLE_TESTS)  
-      if (NOT CURTEST_ONLY_COMPILE)
-        set(SKIP_CUR_TEST "1")
-        # the "x " is a hack which is required to prevent CMake from
-        # evaluating the condition in the string. (which might
-        # evaluate to an empty string even though "${CURTEST_CONDITION}"
-        # is not empty.)
-        if ("x ${CURTEST_CONDITION}" STREQUAL "x ")
-          set(SKIP_CUR_TEST "0")
-        elseif(${CURTEST_CONDITION})
-          set(SKIP_CUR_TEST "0")
-        endif()
+  if (NOT CURTEST_EXE_NAME)
+    set(CURTEST_EXE_NAME ${TestName})
+  endif()
+  if (NOT CURTEST_SOURCES)
+    set(CURTEST_SOURCES ${CURTEST_EXE_NAME}.cc)
+  endif()
+  if (NOT CURTEST_DRIVER_ARGS)
+    set(CURTEST_DRIVER_ARGS --simulation ${CURTEST_EXE_NAME})
+  endif()
+  if (NOT BUILD_TESTING)
+    set(CURTEST_EXCLUDE_FROM_ALL "EXCLUDE_FROM_ALL")
+  endif()
 
-        if (NOT SKIP_CUR_TEST)
-          if (NOT CURTEST_NO_COMPILE)
-            add_executable("${CURTEST_EXE_NAME}" ${CURTEST_EXCLUDE_FROM_ALL} ${CURTEST_SOURCES})
-            target_link_libraries (${CURTEST_EXE_NAME} ${${project}_LIBRARIES})
-          endif()
+  if (NOT CURTEST_ONLY_COMPILE)
+    set(SKIP_CUR_TEST "1")
+    # the "x " is a hack which is required to prevent CMake from
+    # evaluating the condition in the string. (which might
+    # evaluate to an empty string even though "${CURTEST_CONDITION}"
+    # is not empty.)
+    if ("x ${CURTEST_CONDITION}" STREQUAL "x ")
+      set(SKIP_CUR_TEST "0")
+    elseif(${CURTEST_CONDITION})
+      set(SKIP_CUR_TEST "0")
+    endif()
 
-          add_test(NAME ${TestName} 
-            WORKING_DIRECTORY "${PROJECT_BINARY_DIR}"
-            ${DEPENDS_ON}
-            COMMAND ${CMAKE_SOURCE_DIR}/bin/runtest.sh ${CURTEST_DRIVER_ARGS})
-
-          if (CURTEST_DEPENDS)
-            set_tests_properties(${TestName} PROPERTIES DEPENDS "${CURTEST_DEPENDS}")
-          endif()
-          if (CURTEST_PROCESSORS)
-            set_tests_properties(${TestName} PROPERTIES PROCESSORS "${CURTEST_PROCESSORS}")
-          endif()
-        else ()
-          add_test(${TestName} skip_test_dummy)
-        endif()
+    if (NOT SKIP_CUR_TEST)
+      if (NOT CURTEST_NO_COMPILE)
+        add_executable("${CURTEST_EXE_NAME}" ${CURTEST_EXCLUDE_FROM_ALL} ${CURTEST_SOURCES})
+        target_link_libraries (${CURTEST_EXE_NAME} ${${project}_LIBRARIES})
+        add_dependencies("check" "${CURTEST_EXE_NAME}")
       endif()
+
+      add_test(NAME ${TestName}
+        WORKING_DIRECTORY "${PROJECT_BINARY_DIR}"
+        ${DEPENDS_ON}
+        COMMAND ${CMAKE_SOURCE_DIR}/bin/runtest.sh ${CURTEST_DRIVER_ARGS})
+
+      if (CURTEST_DEPENDS)
+        set_tests_properties(${TestName} PROPERTIES DEPENDS "${CURTEST_DEPENDS}")
+      endif()
+      if (CURTEST_PROCESSORS)
+        set_tests_properties(${TestName} PROPERTIES PROCESSORS "${CURTEST_PROCESSORS}")
+      endif()
+    else ()
+      add_test(${TestName} skip_test_dummy)
     endif()
-  endif() # BUILD_TESTING
+  endif()
+
 endmacro()
