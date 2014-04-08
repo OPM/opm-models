OPM/opm-models

commit 30d1543f0cc237e00427d05ff39beed722dffc48

make everything compile with __float128 as Scalar again

ewoms/models/discretefracture/discretefracturefluxvariables.hh
@@ -109,9 +109,11 @@ public:
             // divide the volume flux by two. This is required because
             // a fracture is always shared by two sub-control-volume
             // faces.
-            fractureVolumeFlux_[phaseIdx]
-                = (fractureFilterVelocity_[phaseIdx] * distDirection)
-                  * (fractureWidth_ / 2.0) / scvf.area();
+            fractureVolumeFlux_[phaseIdx] = 0;
+            for (int dimIdx = 0; dimIdx < dimWorld; ++dimIdx)
+                fractureVolumeFlux_[phaseIdx]
+                    = (fractureFilterVelocity_[phaseIdx][dimIdx] * distDirection[dimIdx])
+                      * (fractureWidth_ / 2.0) / scvf.area();
         }
     }
 

ewoms/disc/common/fvbasediscretization.hh
@@ -1230,7 +1230,7 @@ protected:
 
     std::list<VtkBaseOutputModule<TypeTag>*> vtkOutputModules_;
 
-    std::vector<Scalar> dofTotalVolume_;
+    std::vector<double> dofTotalVolume_;
 };
 } // namespace Ewoms
 

ewoms/disc/common/fvbasegradientcalculator.hh
@@ -44,6 +44,7 @@ class FvBaseGradientCalculator
     typedef typename GET_PROP_TYPE(TypeTag, ElementContext) ElementContext;
 
     enum { dim = GridView::dimension };
+    enum { dimWorld = GridView::dimensionworld };
 
     // maximum number of flux approximation points. to calculate this,
     // we assume that the geometry with the most pointsq is a cube.
@@ -77,13 +78,20 @@ public:
             const auto &interiorPos = stencil.subControlVolume(scvf.interiorIndex()).globalPos();
             const auto &exteriorPos = stencil.subControlVolume(scvf.exteriorIndex()).globalPos();
 
-            auto tmp(interiorPos);
-            tmp -= scvf.integrationPos();
-            interiorDistance_[fapIdx] = std::abs(tmp*normal);
+            interiorDistance_[fapIdx] = 0;
+            exteriorDistance_[fapIdx] = 0;
+            for (int dimIdx = 0; dimIdx < dimWorld; ++dimIdx) {
+                interiorDistance_[fapIdx] +=
+                    (interiorPos[dimIdx] - scvf.integrationPos()[dimIdx])
+                    * normal[dimIdx];
 
-            tmp = exteriorPos;
-            tmp -= scvf.integrationPos();
-            exteriorDistance_[fapIdx] = std::abs(tmp*normal);
+                exteriorDistance_[fapIdx] +=
+                    (exteriorPos[dimIdx] - scvf.integrationPos()[dimIdx])
+                    * normal[dimIdx];
+            }
+
+            interiorDistance_[fapIdx] = std::abs(interiorDistance_[fapIdx]);
+            exteriorDistance_[fapIdx] = std::abs(exteriorDistance_[fapIdx]);
         }
     }
 
@@ -138,14 +146,11 @@ public:
         const auto &face = stencil.interiorFace(fapIdx);
         const auto &normal = face.normal();
 
-        Scalar dy =
-            quantityCallback(face.exteriorIndex()) - quantityCallback(face.interiorIndex());
+        Scalar dy = quantityCallback(face.exteriorIndex()) - quantityCallback(face.interiorIndex());
+        Scalar dx = exteriorDistance_[fapIdx] + interiorDistance_[fapIdx];
 
-        Scalar dx =
-            exteriorDistance_[fapIdx] + interiorDistance_[fapIdx];
-
-        quantityGrad = normal;
-        quantityGrad *= dy/dx;
+        for (int dimIdx = 0; dimIdx < dimWorld; ++dimIdx)
+            quantityGrad[dimIdx] = normal[dimIdx]*dy/dx;
     }
 
     /*!

ewoms/models/common/darcyvelocitymodule.hh
@@ -258,7 +258,12 @@ protected:
             }
 
             calculateDarcyVelocity_(phaseIdx);
-            volumeFlux_[phaseIdx] = (filterVelocity_[phaseIdx] * normal);
+
+            Scalar tmp = 0;
+            for (int dimIdx = 0; dimIdx < dimWorld; ++dimIdx)
+                tmp += filterVelocity_[phaseIdx][dimIdx] * normal[dimIdx];
+
+            volumeFlux_[phaseIdx] = tmp;
         }
     }
 

ewoms/models/common/energymodule.hh
@@ -737,7 +737,10 @@ protected:
 
         // scalar product of temperature gradient and scvf normal
         const auto &face = elemCtx.stencil(/*timeIdx=*/0).interiorFace(faceIdx);
-        temperatureGradNormal_ = (face.normal()*temperatureGrad);
+
+        temperatureGradNormal_ = 0;
+        for (int dimIdx = 0; dimIdx < dimWorld; ++dimIdx)
+            temperatureGradNormal_ += (face.normal()[dimIdx]*temperatureGrad[dimIdx]);
 
         const auto &fluxVars = elemCtx.fluxVars(faceIdx, timeIdx);
         const auto &volVarsInside = elemCtx.volVars(fluxVars.interiorIndex(), timeIdx);
@@ -767,7 +770,10 @@ protected:
         DimVector distVec = face.integrationPos();
         distVec -= insideScv.geometry().center();
 
-        Scalar dist = distVec * face.normal();
+        Scalar tmp = 0;
+        for (int dimIdx = 0; dimIdx < dimWorld; ++dimIdx)
+            tmp += distVec[dimIdx] * face.normal()[dimIdx];
+        Scalar dist = tmp;
 
         // if the following assertation triggers, the center of the
         // center of the interior SCV was not inside the element!
@@ -775,8 +781,8 @@ protected:
 
         // calculate the temperature gradient using two-point gradient
         // appoximation
-        temperatureGradNormal_ = (fs.temperature(/*phaseIdx=*/0)
-                                  - fsInside.temperature(/*phaseIdx=*/0)) / dist;
+        temperatureGradNormal_ =
+            (fs.temperature(/*phaseIdx=*/0) - fsInside.temperature(/*phaseIdx=*/0)) / dist;
 
         // take the value for heat conductivity from the interior finite volume
         heatConductivity_ = volVarsInside.heatConductivity();

ewoms/models/common/multiphasebasefluxvariables.hh
@@ -101,7 +101,11 @@ public:
                     continue;
                 }
 
-                if (potentialGrad(phaseIdx) * scvf.normal() > 0) {
+                Scalar tmp = 0;
+                for (int dimIdx = 0; dimIdx < dimWorld; ++dimIdx)
+                    tmp += potentialGrad(phaseIdx)[dimIdx] * scvf.normal()[dimIdx];
+
+                if (tmp > 0) {
                     upstreamScvIdx_[phaseIdx] = this->exteriorIndex();
                     downstreamScvIdx_[phaseIdx] = this->interiorIndex();
                 }

ewoms/models/stokes/stokesboundaryratevector.hh
@@ -106,7 +106,11 @@ public:
             = stencil.subControlVolume(insideScvIdx).geometry().center();
         const auto &scvPos = context.element().geometry().corner(insideScvIdx);
         distVec.axpy(-1, scvPos);
-        Scalar dist = std::abs(distVec * normal);
+
+        Scalar dist = 0.0;
+        for (int dimIdx = 0; dimIdx < dimWorld; ++dimIdx)
+            dist += distVec[dimIdx] * normal[dimIdx];
+        dist = std::abs(dist);
 
         DimVector gradv[dimWorld];
         for (int axisIdx = 0; axisIdx < dimWorld; ++axisIdx) {
@@ -119,7 +123,9 @@ public:
         }
 
         // specify the mass fluxes over the boundary
-        Scalar volumeFlux = (velocity*normal);
+        Scalar volumeFlux = 0;
+        for (int dimIdx = 0; dimIdx < dimWorld; ++dimIdx)
+            volumeFlux += velocity[dimIdx]*normal[dimIdx];
 
         typename FluidSystem::ParameterCache paramCache;
         paramCache.updatePhase(fluidState, phaseIdx);
@@ -140,9 +146,11 @@ public:
             }
 
             // the momentum flux due to viscous forces
-            (*this)[momentum0EqIdx + axisIdx]
-                = -insideVolVars.fluidState().viscosity(phaseIdx)
-                  * (tmp * normal);
+            Scalar tmp2 = 0.0;
+            for (int dimIdx = 0; dimIdx < dimWorld; ++dimIdx)
+                tmp2 += tmp[dimIdx]*normal[dimIdx];
+
+            (*this)[momentum0EqIdx + axisIdx] = -insideVolVars.fluidState().viscosity(phaseIdx) * tmp2;
         }
 
         EnergyModule::setEnthalpyRate(*this, fluidState, phaseIdx, volumeFlux);

commit 09b1164414ff44571633ae76ff113cb80b76d6e5

fix stupid bug introduced in 30d1543f0

ewoms/models/discretefracture/discretefracturefluxvariables.hh
@@ -112,7 +112,7 @@ public:
             fractureVolumeFlux_[phaseIdx] = 0;
             for (int dimIdx = 0; dimIdx < dimWorld; ++dimIdx)
                 fractureVolumeFlux_[phaseIdx]
-                    = (fractureFilterVelocity_[phaseIdx][dimIdx] * distDirection[dimIdx])
+                    += (fractureFilterVelocity_[phaseIdx][dimIdx] * distDirection[dimIdx])
                       * (fractureWidth_ / 2.0) / scvf.area();
         }
     }