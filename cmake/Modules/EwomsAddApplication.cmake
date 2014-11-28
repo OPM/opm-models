457468e4eb8aa885da88f96ff5c362a7d442621a
Andreas Lauser <and@poware.org>
2014-11-28 13:16:07 +0100

    EwomsAddApplication: allow to only compile it with this macro, but do not run tests
    if tests are available for the application, EwomsAddTest() must be
    called explicitly:
    
    ```
    EwomsAddApplication(AppName ...)
    EwomsAddTest(TestName1 EXEC_NAME AppName DEPENDS_ON AppName NO_COMPILE ...)
    EwomsAddTest(TestName2 EXEC_NAME AppName DEPENDS_ON AppName NO_COMPILE ...)
    ...
    ```

--- cmake/Modules/EwomsAddApplication.cmake
+++ cmake/Modules/EwomsAddApplication.cmake
@@ -4,12 +4,9 @@
 # EwomsAddApplication(AppName
 #                     [EXE_NAME AppExecutableName]
 #                     [CONDITION ConditionalExpression]
-#                     [DRIVER_ARGS TestDriverScriptArguments]
-#                     [SOURCES SourceFile1 SourceFile2 ...]
-#                     [PROCESSORS NumberOfRequiredCoresForTest]
-#                     [DEPENDS AppName1 AppName2 ...])
+#                     [SOURCES SourceFile1 SourceFile2 ...])
 include(EwomsAddTest)
 
 macro(EwomsAddApplication AppName)
-  EwomsAddTest(${AppName} ${ARGN} ALWAYS_ENABLE)
+  EwomsAddTest(${AppName} ${ARGN} ALWAYS_ENABLE ONLY_COMPILE)
 endmacro()
