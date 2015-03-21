0832a901d96ebefa4a14162fe7f1ecba1c5ee6d0
Andreas Lauser <and@poware.org>
2015-03-21 15:55:41 +0100

    update the README, INSTALL, etc files in preparation for the next release

--- ewoms/version.hh
+++ ewoms/version.hh
@@ -1,9 +1,9 @@
 /*
  * Current API level (for use with DUNE_VERSION_xxx):
  */
-#define EWOMS_VERSION_MAJOR 2014
+#define EWOMS_VERSION_MAJOR 2015
 #define EWOMS_VERSION_MINOR 04
-#define EWOMS_VERSION_REVISION 0
+#define EWOMS_VERSION_REVISION -1
 
-#define EWOMS_VERSION "2014.04-git"
+#define EWOMS_VERSION "2015.04-git"
 #define EWOMS_CODENAME "Ringo"
