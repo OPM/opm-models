ba6e83c270c26c1910868173c3608a4338c72342
Andreas Lauser <and@poware.org>
2015-05-21 16:18:24 +0200

    add a Ewoms::declval() template function
    this function returns an object given an arbitrary type (which does
    not require to exhibit a default constructor) and is hopefully 100%
    equivalent to std::declval(). This function is useful in conjunction
    with the c++ 2011 decltype() keyword to determine the type of the
    return value of non-static member functions
    
    The only reason to add this function to Ewoms are old compilers like
    GCC 4.4 which do not provide it in their standard library.

---
+++ ewoms/common/declval.hh
@@ -0,0 +1,45 @@
+/*
+  Copyright (C) 2015 by Andreas Lauser
+
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/*!
+ * \file
+ * \brief Provides a std::declval equivalent.
+ *
+ * For compilers like GCC 4.4 which do not feature std::declval in their standard
+ * library.
+ */
+#ifndef EWOMS_DECLVAL_HH
+#define EWOMS_DECLVAL_HH
+
+namespace Ewoms {
+/*!
+ * \brief Template function which returns an object of an arbitrary type.
+ *
+ * This is intended to be used in conjunction with the decltype keyword. It is required
+ * because decltype requires an object as an argument and some objects do not provide a
+ * default constructor. If you try to call the declval function at run time, you'll get a
+ * compiler error.
+ *
+ * This function is equivalent to std::declval, but it is required for compilers like GCC
+ * 4.4 which do not feature std::declval in their standard library.
+ */
+template <class T>
+T &declval();
+} // namespace Ewoms
+
+#endif
