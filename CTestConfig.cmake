6cbadca02034dd215f3ef501acc933f2f09e11f4
Andreas Lauser <and@poware.org>
2015-11-05 16:02:59 +0100

    update the CDash drop site
    the new website does not support CDash yet (and it looks like it never
    will). since the only modules I really care about are opm-material and
    eWoms, let's temporarily move the dropsite to my private site
    (poware.org). The exact URL will probably change once more before the
    next OPM release.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -10,6 +10,6 @@ set(CTEST_PROJECT_NAME "eWoms")
 set(CTEST_NIGHTLY_START_TIME "00:01:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "opm-project.org")
+set(CTEST_DROP_SITE "oldopmweb.poware.org")
 set(CTEST_DROP_LOCATION "/CDash/submit.php?project=eWoms")
 set(CTEST_DROP_SITE_CDASH TRUE)
