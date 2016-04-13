d640259bdc160a6be817e54ed4ba16bf175f9589
Andreas Lauser <and@poware.org>
2016-04-13 11:00:14 +0200

    adapt the grid manager classes to the new opm-parser grid property API

--- applications/ebos/eclpolyhedralgridmanager.hh
+++ applications/ebos/eclpolyhedralgridmanager.hh
@@ -144,7 +144,9 @@ public:
 protected:
     void createGrids_()
     {
-        const std::vector<double>& porv = this->eclState()->getDoubleGridProperty("PORV")->getData();
+        const auto& gridProps = this->eclState()->get3DProperties();
+        const std::vector<double> &porv = gridProps.getDoubleGridProperty("PORV").getData();
+
         grid_ = new Grid(this->deck(), porv);
         cartesianIndexMapper_ = new CartesianIndexMapper(*grid_);
     }
