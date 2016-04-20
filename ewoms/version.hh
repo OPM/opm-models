7fea1c77645b7b2426dab4ae409c76246eb19e41
Andreas Lauser <and@poware.org>
2016-04-20 15:57:30 +0200

    update the version to 2016.10-pre ("Butch")

--- ewoms/version.hh
+++ ewoms/version.hh
@@ -21,11 +21,11 @@
   copyright holders.
 */
 #define EWOMS_VERSION_MAJOR 2016
-#define EWOMS_VERSION_MINOR 04
-#define EWOMS_VERSION_REVISION 0 // -1 means that this is a version from the development branch...
+#define EWOMS_VERSION_MINOR 10
+#define EWOMS_VERSION_REVISION -1 // -1 means that this is a version from the development branch...
 
 #define EWOMS_VERSION_SUFFIX "pre"
-#define EWOMS_VERSION_CODENAME "Yolanda"
+#define EWOMS_VERSION_CODENAME "Butch"
 
 #include <string>
 #include <sstream>
