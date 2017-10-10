c873e8c92da389bc1d6bc4ed2a5241faddfa7630
Andreas Lauser <and@poware.org>
2017-10-10 13:14:29 +0200

    ebos: don't break the downstream build because of the SimulatorParameter mess
    this now works with the unmodified master version of flow from
    opm-simulators. we take the liberty to emit a deprecation warning,
    though. this complicates things quite a bit.

--- ewoms/io/basegridmanager.hh
+++ ewoms/io/basegridmanager.hh
@@ -136,6 +136,7 @@ private:
     const Implementation& asImp_() const
     { return *static_cast<const Implementation*>(this); }
 
+protected:
     Simulator& simulator_;
     std::unique_ptr<GridView> gridView_;
 #if HAVE_DUNE_FEM
