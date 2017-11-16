231fe5e223bc19a9a0a1f80d02916a08db7fca36
hnil <Halvor.M.Nilsen@sintef.no>
2017-11-16 21:24:20 +0100

    Tried to fix serialization of simulator by adding time step to it commented probably unwanted code

--- ewoms/common/simulator.hh
+++ ewoms/common/simulator.hh
@@ -350,11 +350,15 @@ public:
      */
     Scalar timeStepSize() const
     {
+        return timeStepSize_;
+
+        /* This should be handeled by ghe time stepper
         Scalar maximumTimeStepSize =
             std::min(episodeMaxTimeStepSize(),
                      std::max( Scalar(0), endTime() - this->time()));
 
         return std::min(timeStepSize_, maximumTimeStepSize);
+        */
     }
 
     /*!
@@ -835,6 +839,7 @@ public:
             << episodeLength_ << " "
             << startTime_ << " "
             << time_ << " "
+            << timeStepSize_ << " "
             << timeStepIdx_ << " ";
         restarter.serializeSectionEnd();
     }
@@ -856,6 +861,7 @@ public:
             >> episodeLength_
             >> startTime_
             >> time_
+            >> timeStepSize_
             >> timeStepIdx_;
         restarter.deserializeSectionEnd();
     }
