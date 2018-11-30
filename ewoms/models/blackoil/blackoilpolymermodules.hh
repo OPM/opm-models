f5af6b4409b4c8cfbe3d2816ad2533e76134e86b
Kai Bao <kai.bao@sintef.no>
2018-11-30 13:38:20 +0100

    can not convert 1.0 to Evaluation directly
    due to the employment of DynamicEvaluation.
    
    or we should use the tools from ToolBox directly.

--- ewoms/models/blackoil/blackoilpolymermodules.hh
+++ ewoms/models/blackoil/blackoilpolymermodules.hh
@@ -884,14 +884,14 @@ public:
         const Scalar eps = 1e-14;
         // return 1.0 if the polymer has no effect on the water.
         if ( std::abs( (viscosityMultiplier - 1.0) ) < eps){
-            return 1.0;
+            return v0 * 0. + 1.;
         }
 
         const std::vector<Scalar>& shearEffectRefLogVelocity = plyshlogShearEffectRefLogVelocity_[pvtnumRegionIdx];
         auto v0AbsLog = Opm::log(Opm::abs(v0));
         // return 1.0 if the velocity /sharte is smaller than the first velocity entry.
         if (v0AbsLog < shearEffectRefLogVelocity[0])
-            return 1.0;
+            return v0 * 0. + 1.0;
 
         // compute shear factor from input
         // Z = (1 + (P - 1) * M(v) ) / P
